9cc79621fa42b1f7e5165c27db0017c688a76066
lizmery <78451440+lizmery@users.noreply.github.com>
2021-06-30 13:57:14 -0700

    Add files via upload

---
+++ project/javajam.js
@@ -0,0 +1,14 @@
+document.querySelector('.hamburger-menu').addEventListener('click', () => {
+    document.querySelector('.container').classList.toggle('change');
+});
+
+
+
+/*
+const menuIcon = document.querySelector('.menu-toggle');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('change');
+});
+*/
